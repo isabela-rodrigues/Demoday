2525bf5f3def3323b378cdc79867fe6ed77fc5b3
isabela-rodrigues <isabelarodriguescos@gmail.com>
2019-10-19 20:25:25 -0300

    códigos do login e cadastro feitos, nao consegui testar pois estou com problemas no git com o runserver

--- website/views.py
+++ website/views.py
@@ -3,4 +3,46 @@ from django.shortcuts import render
 def index (request):
     return render(request, 'index.html')
 
-# Create your views here.
+def Cadastro(request):
+    if request.method == 'POST':
+        data_usuario = Usuario()
+        data_usuario.email = request.POST['email']
+        data_usuario.senha = request.POST['senha']
+        data_usuario.save()
+
+        data_cadastro = Cadastro()
+        data_cadastro.usuario = data_usuario
+        data_cadastro.nome = request.POST['nome']
+        data_cadastro.sobrenome = request.POST['sobrenome']
+        data_cadastro.endereco = request.POST['endereco']
+        data_cadastro.save()
+
+        args = {
+            'sucesso': 'Você conseguiu campeão! Grite: Alucinação!'
+        }
+
+        return render(request, 'login.html', args)
+
+    return render(request, 'index.html')
+
+
+def login(request):
+    if request.method == "POST":
+        formulario_email = request.POST['email']
+        formulario_senha = request.POST['senha']
+
+        usuario_logado = Cadastro.objects.filter(usuario__email = formulario_email,
+                                              usuario__senha = formulario_senha).first()
+
+        if usuario_logado is not None:
+            args = {
+                'dados': usuario_logado
+            }
+            return render(request, 'index.html', args)
+        else:
+            args = {
+                'msg': 'Credenciais inválidas, tente novamente'
+            }
+            return render(request, 'login.html', args)
+
+    return render(request, 'login.html')
